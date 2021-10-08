11e1f19d39827054b842265fff592a509fbb54f3
marcosaccarola <marco.sacca.ads@gmail.com>
2021-10-08 13:10:34 +0200

    POST product

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -13,5 +13,14 @@ products.get('/',async(req,res,next)=>{
         next(error)
     }
 })
+products.post('/',async(req,res,next)=>{
+    try {
+        const newProduct=await productModel(req.body)
+        const{_id}=await newProduct.save()
+        res.status(201).send({_id})
+    } catch (error) {
+        next(error)
+    }
+})
 
 export default products
