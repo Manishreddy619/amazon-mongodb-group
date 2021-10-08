2c4696c846530c2986a923c320c82d4fc85a54b8
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-10-08 12:14:55 +0200

    first commit

---
+++ src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import listEndpoints from 'express-list-endpoints';
+import cors from 'cors';
+import {
+	notFoundHandler,
+	badRequestHandler,
+	genericErrorHandler,
+} from './errorHandlers.js';
+
+const server = express();
+const port = 3001;
+
+/////////////middlewares
+server.use(cors());
+server.use(express.json());
+////Routes comes here
+
+// ************************** ERROR HANDLERS ***************************
+
+server.use(notFoundHandler);
+server.use(badRequestHandler);
+server.use(genericErrorHandler);
+
+mongoose.connect(process.env.MONGO_CONNECTION); ////mongodb+srv://manish:mani@cluster0.gycza.mongodb.net/test
+
+mongoose.connection.on('connected', () => {
+	console.log('Successfully connected to Mongo!');
+	server.listen(port, () => {
+		console.table(listEndpoints(server));
+		console.log(`Server running on port ${port}`);
+	});
+});
+
+mongoose.connection.on('error', (err) => {
+	console.log(err);
+});
